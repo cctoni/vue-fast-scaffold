c5f7cac5252d7568f6ebc4b59cedd5744ccd4978
Toni Haupt <toni.haupt@codecentric.de>
2018-03-27 13:57:07 +0200

    add multiple exports for the vue basics API

---
+++ vueBasics.js
@@ -0,0 +1,78 @@
+// data options
+exports.data = `
+    data() {
+        return {}
+    }
+`
+
+exports.directives = `
+    directives: {}
+`
+
+exports.filters = `
+    filters: {}
+`
+
+exports.components = `
+    components: {}
+`
+
+exports.props = `
+    props: {}
+`
+
+exports.computed = `
+    computed: {}
+`
+
+exports.methods = `
+    methods: {}
+`
+
+exports.watch = `
+    watch: {}
+`
+
+// vue lifecycle hooks wich you can read more about on https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
+exports.beforeCreate = `
+    beforeCreate() {}
+`
+
+exports.created = `
+    created() {}
+`
+
+exports.beforeMount = `
+    beforeMount() {}
+`
+
+exports.mounted = `
+    mounted() {}
+`
+
+exports.beforeUpdate = `
+    beforeUpdate() {}
+`
+
+exports.updated = `
+    updated() {}
+`
+
+exports.activated = `
+    activated() {}
+`
+
+exports.deactivated = `
+    deactivated() {}
+`
+
+exports.beforeDestroy =  `
+    beforeDestroy() {}
+`
+
+exports.detroyed = `
+    destroyed() {}
+`
+
+
+
